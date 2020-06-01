4a626dcc2b33b0de8add0dd7c1d4a505d44d7a52
asmith26 <asmith26@users.noreply.github.com>
2020-06-01 19:18:34 +0100

    Change how .train_validation_split works

--- pandas_toolkit/ml/tests/test_ml.py
+++ pandas_toolkit/ml/tests/test_ml.py
@@ -1,6 +1,5 @@
 import unittest
 
-import numpy as np
 import pandas as pd
 
 import pandas_toolkit.ml
@@ -20,38 +19,40 @@ class TestStandardScaler(unittest.TestCase):
 class TestTrainValidationSplit(unittest.TestCase):
     def test_with_sensible_is_validation_frac(self):
         df = pd.DataFrame({"x": [0, 1, 2], "y": [0, 1, 2]}, index=[0, 1, 2])
+        actual_df_train, actual_df_validation = df.ml.train_validation_split(train_frac=2/3, random_seed=42)
 
-        np.random.seed(1)
-        actual_s = df.ml.train_validation_split(is_validation_frac=2 / 3)
+        expected_df_train = pd.DataFrame({"x": [0, 1], "y": [0, 1]}, index=[0, 1])
+        expected_df_validation = pd.DataFrame({"x": [2], "y": [2]}, index=[2])
 
-        expected_s = pd.Series([1, 0, 1], dtype=np.int8)
-
-        pd.testing.assert_series_equal(expected_s, actual_s, check_exact=True)
+        pd.testing.assert_frame_equal(expected_df_train, actual_df_train, check_exact=True)
+        pd.testing.assert_frame_equal(actual_df_validation, expected_df_validation, check_exact=True)
 
     def test_with_not_perfectly_divisible_is_validation_frac(self):
         df = pd.DataFrame({"x": [0, 1, 2], "y": [0, 1, 2]}, index=[0, 1, 2])
+        actual_df_train, actual_df_validation = df.ml.train_validation_split(train_frac=0.5, random_seed=42)
 
-        np.random.seed(1)
-        actual_s = df.ml.train_validation_split(is_validation_frac=0.5)
+        expected_df_train = pd.DataFrame({"x": [0, 1], "y": [0, 1]}, index=[0, 1])
+        expected_df_validation = pd.DataFrame({"x": [2], "y": [2]}, index=[2])
 
-        expected_s = pd.Series([1, 0, 0], dtype=np.int8)
-
-        pd.testing.assert_series_equal(expected_s, actual_s, check_exact=True)
+        pd.testing.assert_frame_equal(expected_df_train, actual_df_train, check_exact=True)
+        pd.testing.assert_frame_equal(actual_df_validation, expected_df_validation, check_exact=True)
 
     def test_with_is_validation_frac_eq_1(self):
         df = pd.DataFrame({"x": [0, 1, 2], "y": [0, 1, 2]}, index=[0, 1, 2])
+        actual_df_train, actual_df_validation = df.ml.train_validation_split(train_frac=1, random_seed=42)
 
-        actual_s = df.ml.train_validation_split(is_validation_frac=1)
+        expected_df_train = pd.DataFrame({"x": [0, 1, 2], "y": [0, 1, 2]}, index=[0, 1, 2])
+        expected_df_validation = pd.DataFrame({"x": [], "y": []}, dtype="int64")
 
-        expected_s = pd.Series([1, 1, 1], dtype=np.int8)
-
-        pd.testing.assert_series_equal(expected_s, actual_s, check_exact=True)
+        pd.testing.assert_frame_equal(expected_df_train, actual_df_train, check_exact=True)
+        pd.testing.assert_frame_equal(actual_df_validation, expected_df_validation, check_exact=True)
 
     def test_with_is_validation_frac_eq_0(self):
         df = pd.DataFrame({"x": [0, 1, 2], "y": [0, 1, 2]}, index=[0, 1, 2])
+        actual_df_train, actual_df_validation = df.ml.train_validation_split(train_frac=0, random_seed=42)
 
-        actual_s = df.ml.train_validation_split(is_validation_frac=0)
+        expected_df_train = pd.DataFrame({"x": [], "y": []}, dtype="int64")
+        expected_df_validation = pd.DataFrame({"x": [0, 1, 2], "y": [0, 1, 2]}, index=[0, 1, 2])
 
-        expected_s = pd.Series([0, 0, 0], dtype=np.int8)
-
-        pd.testing.assert_series_equal(expected_s, actual_s, check_exact=True)
+        pd.testing.assert_frame_equal(expected_df_train, actual_df_train, check_exact=True)
+        pd.testing.assert_frame_equal(actual_df_validation, expected_df_validation, check_exact=True)
