Add apply_df_train_transform method to fix training validation split scaling issue.

pandas_toolkit/ml/__init__.py
@@ -1,14 +1,65 @@
-from typing import Tuple
+from typing import Callable, Dict, List, Tuple
 
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 
+class SklearnObject(object):
+    @staticmethod
+    def transform(x: np.ndarray) -> np.ndarray:  # pragma: no cover
+        pass
+
+
+class MLTransform(object):
+    def __init__(self, column_name: str, sklearn_object: SklearnObject):
+        self.column_name = column_name
+        self.sklearn_object = sklearn_object
+
+
 @pd.api.extensions.register_dataframe_accessor("ml")
 class MachineLearningAccessor:
     def __init__(self, df: pd.DataFrame):
         self._df = df
+        self.transforms: Dict[str, MLTransform] = {}  # format: {"transform name": MLTransform}
+
+    def apply_df_train_transform(self, ml_transform: MLTransform) -> pd.Series:
+        """
+        **Parameters**
+        > **ml_transform:** `pandas_toolkit.ml.MLTransform` object containing transform to apply and column name to
+          apply it (normally via e.g. `df_train.ml.transforms["standard_scaler"]`).
+
+        **Returns**
+        > Transformed featured using e.g. df_train data statistics.
+
+        Examples
+        ```python
+        >>> df_train = pd.DataFrame({"x": [0, 1],
+                                     "y": [0, 1]},
+                                     index=[0, 1])
+        >>> df_validation = pd.DataFrame({"x": [2],
+                                          "y": [2]},
+                                          index=[0])
+        >>> df_train["standard_scaler_x"] = df_train.ml.standard_scaler(column="x")
+        >>> df_train["standard_scaler_x"]
+        pd.Series([-1, 1])
+
+        >>> df_train.ml.transforms
+        {'standard_scaler': <pandas_toolkit.ml.MLTransform object at 0x7f1af20f0af0>}
+
+        >>> df_validation["standard_scaler_x"] = \\
+                df_validation.ml.apply_df_train_transform(df_train.ml.transforms["standard_scaler"])
+        >>> df_validation["standard_scaler_x"]
+        pd.Series([3])
+        ```
+        """
+        column = ml_transform.column_name
+        sklearn_object = ml_transform.sklearn_object
+
+        s = self._df[column]
+        arr_transformed_col: np.ndarray = sklearn_object.transform(s.values.reshape(-1, 1))
+        s_transformed_col = pd.Series(data=arr_transformed_col.flatten(), index=self._df.index, dtype=s.dtype)
+        return s_transformed_col
 
     def standard_scaler(self, column: str) -> pd.Series:
         """
@@ -20,6 +71,11 @@ class MachineLearningAccessor:
           [scikit-learn](https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html)):
           `z = (x - u) / s` (`u` := mean of training samples, `s` := standard deviation of training samples).
 
+        **Side Effects**
+        > Updates the `df.ml.transforms` dictionary with key "standard_scaler" and value
+          `pandas_toolkit.ml.MLTransform` corresponding to the column name and fitted
+          `sklearn.preprocessing.StandardScaler` object.
+
         Examples
         ```python
         >>> df = pd.DataFrame({"x": [0, 1],
@@ -28,12 +84,17 @@ class MachineLearningAccessor:
         >>> df["standard_scaler_x"] = df.ml.standard_scaler(column="x")
         >>> df["standard_scaler_x"]
         pd.Series([-1, 1])
+
+        >>> df.ml.transforms
+        {'standard_scaler': <pandas_toolkit.ml.MLTransform object at 0x7f1af20f0af0>}
         ```
         """
         s = self._df[column]
         scaler = StandardScaler()
         arr_scaled_col: np.ndarray = scaler.fit_transform(s.values.reshape(-1, 1))
         s_scaled_col = pd.Series(data=arr_scaled_col.flatten(), index=self._df.index, dtype=s.dtype)
+
+        self.transforms["standard_scaler"] = MLTransform(column_name=column, sklearn_object=scaler)
         return s_scaled_col
 
     def train_validation_split(self, train_frac: float, random_seed: int = None) -> Tuple[pd.DataFrame, pd.DataFrame]: